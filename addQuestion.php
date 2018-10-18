ff4705935a3248120d7db7c32b60581935f8309d
Gorka Garitazelaia <gorkagari@gmail.com>
2018-10-18 10:15:56 +0200

    master

--- addQuestion.php
+++ addQuestion.php
@@ -4,27 +4,42 @@
 <head>
 	<title> MySQL konexioa </title>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<?php //include dbkonfiguratu.php
-		$esteka = mysqli_connect(“localhost”, “root", "jlgg", “quiz");
-		// ("localhost", "root", "", “proba");
-		if (!$esteka){
-			echo “Hutsegitea MySQLra konetatzerakoan. “ .” . PHP_EOL;
-			echo "errno depurazio akatsa: " .mysqli_connect_errno().PHP_EOL;
-			echo "error depurazio akatsa: " .mysqli_connect_error().PHP_EOL;
-			exit;
-		}else{
-			var id = mysql_query($esteka, "SELECT COUNT(*) FROM questions" )
-			id++;
-			$sql = mysqli_query($esteka, "INSERT INTO questions(id,email,galdera,okerra1,okerra2,okerra3,zailtasuna,gaia) VALUES (".id.",".$_POST['email'].",".$_POST['galdera'].",".$_POST['okerra1'].",".$_POST['okerra2'].",".$_POST['okerra3'].",".$_POST['zailtasuna'].",".$_POST['gaia'].")")
-		}
-		
-		
-		// Konexioa itxi
-		mysqli_close($esteka);
-	?>
+	
 </head>
 <body>
-	<h1> MySQL </h1>
+	<?php include 'dbkonfiguratu.php';
+		$esteka = mysqli_connect($zerbitzaria, $erabiltzailea, $gakoa, $db);
+		// ("localhost", "root", "", “proba");
+		if (!$esteka){
+			echo "Hutsegitea MySQLra konetatzerakoan";
+			echo "errno depurazio akatsa: " .mysqli_connect_errno().PHP_EOL;
+			echo "error depurazio akatsa: " .mysqli_connect_error().PHP_EOL;
+			$p_text = 'Errore bat egon da';
+			exit;
+		}else{
+			$sql = mysqli_query($esteka, "INSERT INTO questions(email,galdera,okerra1,okerra2,okerra3,zailtasuna,gaia) VALUES (".$_REQUEST['email'].",".$_REQUEST['galdera'].",".$_REQUEST['okerra1'].",".$_REQUEST['okerra2'].",".$_REQUEST['okerra3'].",".$_REQUEST['zailtasuna'].",".$_REQUEST['gaia'].")");
+			    
+		}
+		if($sql){
+		    $p_text = 'Ondo joan da';
+		}else{
+		    $p_text = 'Errore bat egon da';
+		}
+		$dom = new DOMDocument('1.0', 'utf-8');
+        $h = $dom->createElement('h2', $p_text);//Create new <p> tag with text
+        $dom->appendChild($h);//Add the p tag to document
+        echo $dom->saveXML();
+		// Konexioa itxi
+		mysqli_close($esteka);
 	
+		
+	?>
+	
+	<div id=d> </div>
+	<div id=links>
+			<a  href="./datubase.php"> Datu basea ikusi </a><br></br>
+			<a  href="./addQuestion.html"> Galdera berri bat sartu </a><br></br>
+			<a  href="./layout.html"> Itzuli menura </a>
+	</div>
 </body>
 </html>
--- dbkonfiguratu.php
+++ dbkonfiguratu.php
@@ -5,22 +5,21 @@
 		<title> konfigurazioa </title>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<?php
-			$local = 0 //1 hodeiko datuentzat
+			$local = 0; //1 hodeiko datuentzat
 			if ($local){
-				$zerbitzaria = "localhost"
-				$erabiltzailea = "id7193466_josugorka"
-				$gakoa = "porfinputointernet5semanas_"
-				$db = "id7193466_quiz"
+				$zerbitzaria = "localhost";
+				$erabiltzailea = "id7193466_josugorka";
+				$gakoa = "porfinputointernet5semanas_";
+				$db = "id7193466_quiz";
 			}else{
-				$zerbitzaria = "localhost"
-				$erabiltzailea = "root"
-				$gakoa = ""
-				$db = "quiz"
+				$zerbitzaria = "localhost";
+				$erabiltzailea = "root";
+				$gakoa = "";
+				$db = "quiz";
 			}
 		?>
 	</head>
-	<body>
-
-
-	</body>
+	
+	<body></body>
+	
 </html>
